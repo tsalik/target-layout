6ea629d1b244919e72f085b620b4d46e8d9efeb6
ktsal <tsalikispk@gmail.com>
2019-05-18 15:47:57 +0300

    Adds AndroidX

--- TargetLayout/targetlayout/src/main/java/com/tsalik/targetlayout/TargetLayout.java
+++ TargetLayout/targetlayout/src/main/java/com/tsalik/targetlayout/TargetLayout.java
@@ -11,7 +11,7 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LevelListDrawable;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.MotionEvent;
